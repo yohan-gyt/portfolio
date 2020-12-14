518722e605aad4cce5aea27328fb026c29f34d36
Yohan Guyot <61965649+Yohan-gyt@users.noreply.github.com>
2020-12-14 06:08:19 +0100

    accueil fini + bug incompréhensible flex column safari
    num-line n'est pas bien placé sur safari mais ça se replace bien quand on inspecte l'élément et qu'on coche et décoche le flex-direction... JE CAPTE RIEN !!!

---
+++ assets/js/project-hover.js
@@ -0,0 +1,98 @@
+$(".project-img img").hover(
+    function () {
+        $(this).addClass("hover");
+    },
+    function () {
+        $(this).removeClass("hover");
+    }
+);
+
+$(".project-img-right img").hover(
+    function () {
+        $(this).addClass("hover");
+    },
+    function () {
+        $(this).removeClass("hover");
+    }
+);
+
+$(".slyde-title").hover(
+    function () {
+        $("#slyde").addClass("hover");
+    },
+    function () {
+        $("#slyde").removeClass("hover");
+    }
+);
+
+$(".mauvaise-rencontre-title").hover(
+    function () {
+        $("#mauvaise-rencontre").addClass("hover");
+    },
+    function () {
+        $("#mauvaise-rencontre").removeClass("hover");
+    }
+);
+
+$(".jpo-2021-title").hover(
+    function () {
+        $("#jpo-2021").addClass("hover");
+    },
+    function () {
+        $("#jpo-2021").removeClass("hover");
+    }
+);
+
+$(".cv-video-title").hover(
+    function () {
+        $("#cv-video").addClass("hover");
+    },
+    function () {
+        $("#cv-video").removeClass("hover");
+    }
+);
+
+$(".mmi-town-title").hover(
+    function () {
+        $("#mmi-town").addClass("hover");
+    },
+    function () {
+        $("#mmi-town").removeClass("hover");
+    }
+);
+
+$(".cv-numerique-title").hover(
+    function () {
+        $("#cv-numerique").addClass("hover");
+    },
+    function () {
+        $("#cv-numerique").removeClass("hover");
+    }
+);
+
+$(".redesign-netflix-title").hover(
+    function () {
+        $("#redesign-netflix").addClass("hover");
+    },
+    function () {
+        $("#redesign-netflix").removeClass("hover");
+    }
+);
+
+$(".portrait-chinois-title").hover(
+    function () {
+        $("#portrait-chinois").addClass("hover");
+    },
+    function () {
+        $("#portrait-chinois").removeClass("hover");
+    }
+);
+
+$(".jpo-2020-title").hover(
+    function () {
+        $("#jpo-2020").addClass("hover");
+    },
+    function () {
+        $("#jpo-2020").removeClass("hover");
+    }
+);
