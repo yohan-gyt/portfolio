7552014b7cff6d852cd3c591b5383c9efe87826b
Yohan Guyot <61965649+Yohan-gyt@users.noreply.github.com>
2020-12-23 22:56:24 +0100

    ça avance

--- assets/js/project-hover.js
+++ assets/js/project-hover.js
@@ -1,99 +1,93 @@
 $(".slyde").hover(
     function () {
-        $("#slyde-img").addClass("hover");
-        $(".slyde-title").addClass("hover");
+        $("#slyde-img, .slyde-title").addClass("hover");
     },
     function () {
-        $("#slyde-img").removeClass("hover");
-        $(".slyde-title").removeClass("hover");
+        $("#slyde-img, .slyde-title").removeClass("hover");
     }
 );
 
 $(".mauvaise-rencontre").hover(
     function () {
-        $("#mauvaise-rencontre-img").addClass("hover");
-        $(".mauvaise-rencontre-title").addClass("hover");
+        $("#mauvaise-rencontre-img, .mauvaise-rencontre-title").addClass("hover");
     },
     function () {
-        $("#mauvaise-rencontre-img").removeClass("hover");
-        $(".mauvaise-rencontre-title").removeClass("hover");
+        $("#mauvaise-rencontre-img, .mauvaise-rencontre-title").removeClass("hover");
     }
 );
 
 $(".jpo-2021").hover(
     function () {
-        $("#jpo-2021-img").addClass("hover");
+        $("#jpo-2021-img, .jpo-2021-title").addClass("hover");
         $(".jpo-2021-title").addClass("hover");
     },
     function () {
-        $("#jpo-2021-img").removeClass("hover");
-        $(".jpo-2021-title").removeClass("hover");
+        $("#jpo-2021-img, .jpo-2021-title").removeClass("hover");
     }
 );
 
 $(".cv-video").hover(
     function () {
-        $("#cv-video-img").addClass("hover");
-        $(".cv-video-title").addClass("hover");
+        $("#cv-video-img, .cv-video-title").addClass("hover");
     },
     function () {
-        $("#cv-video-img").removeClass("hover");
-        $(".cv-video-title").removeClass("hover");
+        $("#cv-video-img, .cv-video-title").removeClass("hover");
     }
 );
 
 $(".mmi-town").hover(
     function () {
-        $("#mmi-town-img").addClass("hover");
-        $(".mmi-town-title").addClass("hover");
+        $("#mmi-town-img, .mmi-town-title").addClass("hover");
     },
     function () {
-        $("#mmi-town-img").removeClass("hover");
-        $(".mmi-town-title").removeClass("hover");
+        $("#mmi-town-img, .mmi-town-title").removeClass("hover");
     }
 );
 
 $(".cv-numerique").hover(
     function () {
-        $("#cv-numerique-img").addClass("hover");
-        $(".cv-numerique-title").addClass("hover");
+        $("#cv-numerique-img, .cv-numerique-title").addClass("hover");
     },
     function () {
-        $("#cv-numerique-img").removeClass("hover");
-        $(".cv-numerique-title").removeClass("hover");
+        $("#cv-numerique-img, .cv-numerique-title").removeClass("hover");
     }
 );
 
 $(".redesign-netflix").hover(
     function () {
-        $("#redesign-netflix-img").addClass("hover");
-        $(".redesign-netflix-title").addClass("hover");
+        $("#redesign-netflix-img, .redesign-netflix-title").addClass("hover");
     },
     function () {
-        $("#redesign-netflix-img").removeClass("hover");
-        $(".redesign-netflix-title").removeClass("hover");
+        $("#redesign-netflix-img, .redesign-netflix-title").removeClass("hover");
     }
 );
 
 $(".portrait-chinois").hover(
     function () {
-        $("#portrait-chinois-img").addClass("hover");
-        $(".portrait-chinois-title").addClass("hover");
+        $("#portrait-chinois-img, .portrait-chinois-title").addClass("hover");
     },
     function () {
-        $("#portrait-chinois-img").removeClass("hover");
-        $(".portrait-chinois-title").removeClass("hover");
+        $("#portrait-chinois-img, .portrait-chinois-title").removeClass("hover");
     }
 );
 
 
 $(".jpo-2020").hover(
     function () {
-        $("#jpo-2020-img").addClass("hover");
-        $(".jpo-2020-title").addClass("hover");
+        $("#jpo-2020-img, .jpo-2020-title").addClass("hover");
     },
     function () {
-        $("#jpo-2020-img").removeClass("hover");
-        $(".jpo-2020-title").removeClass("hover");
+        $("#jpo-2020-img, .jpo-2020-title").removeClass("hover");
+    }
+);
+
+$(".close").hover(
+    function () {
+        $(".animated-line").addClass("line-2-hover");
+        $(".animated-line").removeClass("line-2");
+    },
+    function () {
+        $(".animated-line").addClass("line-2");
+         $(".animated-line").removeClass("line-2-hover");
     }
 );
